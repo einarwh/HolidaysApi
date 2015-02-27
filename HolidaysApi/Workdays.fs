8f3df3cdbf3262a63f6f12b2685db91cd6ab29bc
Einar W. Høst <einarwh@gmail.com>
2015-02-27 12:36:52 +0100

    More declarative-like.

--- HolidaysApi/Workdays.fs
+++ HolidaysApi/Workdays.fs
@@ -3,11 +3,11 @@
 open System
 
 module Workdays =
+    let IsDayOff country date =
+        Dates.IsSunday date || Dates.IsSaturday date || Holidays.IsHoliday country date
+
     let IsWorkDay country date =
-        if Dates.IsSunday date then false
-        elif Dates.IsSaturday date then false
-        elif Holidays.IsHoliday country date then false
-        else true
+        !(IsDayOff country date)
 
     let rec PreviousWorkday country (date:DateTime) =
         let previousDay = date.AddDays(-1.0)
